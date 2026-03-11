13311917d4f7c2e1787c78165dac67812a03420d
rnoguer22 <rnogugon@myuax.com>
2026-03-11 22:13:19 +0100

    Matriz de correlacion. Eliminamos columnas redundantes

--- code/anomalias.py
+++ code/anomalias.py
@@ -21,6 +21,7 @@ class Anomalias:
         self.repo_path = os.getenv('REPOSITORY_PATH')
         self.data_path = os.path.join(self.repo_path, 'data/csv/')
         self.df_list = []
+        self.drop_columns = []
         self.scaler = MinMaxScaler()
 
     # Metodo para obtener un dataframe a partir de los datos del CICIDS2017
@@ -36,12 +37,12 @@ class Anomalias:
         return self.df
     
     # Metodos para obtener los dataframes unicamente con datos benignos o malignos, y el scaler
-    def get_df_benign(self):
-        self.df_benign = self.df[self.df[' Label'] == 'BENIGN']
+    def get_df_benign(self, df):
+        self.df_benign = df[df[' Label'] == 'BENIGN']
         return self.df_benign
     
-    def get_df_malign(self):
-        self.df_malign = self.df[self.df[' Label'] != 'BENIGN']
+    def get_df_malign(self, df):
+        self.df_malign = df[df[' Label'] != 'BENIGN']
         return self.df_malign
     
     def get_scaler(self):
@@ -66,7 +67,7 @@ class Anomalias:
             filename = 'class_distribution_benign_vs_malign.png'
 
         # Hacemos un countplot para ver el número de valores de cada opcion posible en la columna ' Label'
-        sns.countplot(data=df_plot, x=' Label', order=df_plot[' Label'].value_counts().index, palette='Set2')
+        sns.countplot(data=df_plot, x=' Label', order=df_plot[' Label'].value_counts().index, palette='bright')
         plt.title(title)
         plt.ylabel('Número de logs')
         plt.tight_layout()
@@ -75,29 +76,51 @@ class Anomalias:
         plt.show()
 
 
+    def get_corr_matrix_from_df(self, df):
+        # Seleccionamos solo las 'n' columnas con mayor varianza ya que son demasiadas columnas y no podemos ponerlas todas (para que el grafico sea legible, porque si ponemos las 80 columnas no veriamos nada)
+        df_numeric = df.drop(columns=[' Label'])
+        # Eliminamos columnas con varianza cero (constantes)
+        df_numeric = df_numeric.loc[:, df_numeric.var() > 0]        
+        # Obtneemos la matriz de correlacion
+        corr_matrix = df_numeric.corr()
+        return corr_matrix    
+
+
 
     # Metodo para ver la matriz de correlacion
-    def plot_correlation_matrix(self, n_features=20, save=True):
-        # Seleccionamos solo las 'n' columnas con mayor varianza ya que son demasiadas columnas y no podemos ponerlas todas (para que el grafico sea legible, porque si ponemos las 80 columnas no veriamos nada)
-        df_numeric = self.df.drop(columns=[' Label'])
-        # Eliminamos columnas con varianza cero (constantes)
-        df_numeric = df_numeric.loc[:, df_numeric.var() > 0]
-        
-        top_features = df_numeric.var().sort_values(ascending=False).head(n_features).index
-        # Obtneemos la matriz con las distintas correlaciones
-        corr_matrix = df_numeric[top_features].corr()
-
-        # Mostramos la matriz en un grafico
-        plt.figure(figsize=(12, 10))
-        sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', linewidths=0.5)
-        plt.title(f'Matriz de Correlación (Top {n_features} variables con mayor varianza)')
+    def plot_correlation_matrix(self, corr_matrix, filename, size, save=True):
+        plt.figure(figsize=(size, size))
+        sns.heatmap(corr_matrix, annot=False, cmap='viridis', square=True)
+        plt.title(f'Matriz de Correlación')
+        plt.xticks(rotation=90)
         plt.tight_layout()
         if save:
-            plt.savefig(os.path.join(self.repo_path, 'img/code/matriz_correlacion.png'))
+            plt.savefig(os.path.join(self.repo_path, 'img/code', filename))
         plt.show()
 
 
-    
+
+    # Usamos otro metodo para limpiar las correlaciones que sean muy altas, y mostrar la matriz limpia
+    def get_df_corr_cleaned(self, df, corr_matrix, umbral):
+        # La matriz de correlacion es simetrica, asi que vamos a seleecionar uniamente el triangulo superior (podriamos elegir tambien el inferior)
+        # k=0 es la diagonal, si queremos todo lo que hay por encima escogemos k=1. Si no, k=-1
+        upper_triangle = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+
+        drop_columns = []
+        for column in upper_triangle.columns:
+            # Al seleccionar el triangulo inferior nos aseguramos que solo se elimine una de los variables altamente correlacionadas entre si, y no ambas columnas
+            if any(upper_triangle[column] >= umbral):
+                drop_columns.append(column)
+
+        # Eliminamos las columnas correspondientes
+        print(f'Se eliminaran {len(drop_columns)} columnas: {drop_columns}')
+        df = df.drop(drop_columns, axis=1)  
+        return df
+               
+
+
+
+
     # Metodo para hacer una pequeña limpieza del dataset (ya que los datos estan previamente limpiados por el CIC)
     def clean_df(self, df_to_clean):
         # Eliminaoms la columna label porque la red solo entiende valores numericos, y eliminamos algunos valores que aparecen como infinitos en el df
@@ -137,3 +160,19 @@ class Anomalias:
             verbose=1
         )
         return autoencoder, history
+    
+
+
+
+
+if __name__ == '__main__':
+
+    anomalias = Anomalias()
+    df = anomalias.get_data()
+    corr_matrix_df = anomalias.get_corr_matrix_from_df(df)
+    anomalias.plot_correlation_matrix(corr_matrix_df, 'matriz_correlacion.png', 19)
+
+    df_corr_cleaned = anomalias.get_df_corr_cleaned(df, corr_matrix_df, 0.95)
+    print('\n', df_corr_cleaned, '\n')
+    corr_matrix_df_cleaned = anomalias.get_corr_matrix_from_df(df_corr_cleaned)
+    anomalias.plot_correlation_matrix(corr_matrix_df_cleaned, 'matriz_correlacion_cleaned.png', 14)
